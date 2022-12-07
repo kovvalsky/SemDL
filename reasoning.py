29a3a5595e5a603469e726b82bb66bcbcf2069b2
Fibo Kowalsky <kovvalsky@users.noreply.github.com>
2022-12-07 14:13:16 +0100

    Update reasoning.py

--- reasoning.py
+++ reasoning.py
@@ -11,7 +11,7 @@ import re
 
 #########################################################
 # follows terminology at https://en.wikipedia.org/wiki/Syllogism
-def gen_syllogism(M, S, P, neg="not", types="aeio", figures="1234"):
+def gen_syllogism(M, S, P, neg="not", types="aeio", figures="1234", annotated=False):
     """
     Returns a generator that produces syllogisms. Each syllogism is a pair of
     its id (consisting of the figure and sentence types) and a tuple of three
@@ -27,6 +27,18 @@ def gen_syllogism(M, S, P, neg="not", types="aeio", figures="1234"):
     sel_figures = tuple( (f, v) for (f, v) in sorted(all_figures.items()) if f in figures )
     sel_types = tuple( (t, v) for (t, v) in sorted(all_types.items()) if t in types ) 
 
+    # labeling the syllogisms
+    entailment = set('f1-aaa f1-aii f1-eae f1-eio f2-aee f2-aoo f2-eae f2-eio f3-aii f3-eio f3-iai f3-oao f4-aee f4-eio f4-iai'.split())
+    # contradictions can be deduced based on the negation of the conclusions
+    neg_map = str.maketrans("aeio", "oiea")
+    contradiction = set([ f"{e[:-1]}{e[-1].translate(neg_map)}" for e in entailmen ])
+    
+    # Get label based on the name of a syllogism
+    def get_label(name):
+        if name in entailment: return 'entailment'
+        if name in contradiction: return 'contradiction'
+        return 'neutral'
+    
     d = {'M':M, 'S':S, 'P':P}
     # generation by looping over figures
     for f, (p1, p2) in sel_figures:
@@ -41,4 +53,6 @@ def gen_syllogism(M, S, P, neg="not", types="aeio", figures="1234"):
                     prem1 = s1.format(*p1)
                     prem2 = s2.format(*p2)
                     con = s.format(*p)
-                    yield f"f{f}-{t1}{t2}{t}", (prem1.format(**d), prem2.format(**d), con.format(**d))
+                    name = f"f{f}-{t1}{t2}{t}"
+                    label = f"-{get_label(name)}" if annotated else "" 
+                    yield f"{name}{label}", (prem1.format(**d), prem2.format(**d), con.format(**d))
